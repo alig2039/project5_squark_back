64a45433a45e8b016f7871a653d593c0a66bafb3
alig2039 <alig2039@yahoo.com>
2022-10-22 01:07:36 +0300

    debug changed for testing

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -17,8 +17,9 @@ SECRET_KEY = config('SECRET_KEY')
 IS_HEROKU = "DYNO" in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU:
-    DEBUG = True
+# if not IS_HEROKU:
+#     DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
