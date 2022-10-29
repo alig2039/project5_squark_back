e5a31984f9b9d218d9bb62b4b4c4be501e50521b
alig2039 <alig2039@yahoo.com>
2022-10-29 20:06:24 +0300

    moved db credentials to env file

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -84,9 +84,13 @@ ASGI_APPLICATION = 'mainproject.asgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+       "ENGINE" : "django.db.backends.postgresql_psycopg2",
+       'NAME': config("NAME", default=''),
+       'USER': config("USER", default=''),
+       'PASSWORD': config("PASSWORD", default=''),
+       'HOST': config("HOST", default=''),
+       'PORT': '5432'
     }
 }
 
