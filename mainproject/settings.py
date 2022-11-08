fd2930bf3f39932966e0f42b0b89729badafd41b
alig2039 <alig2039@yahoo.com>
2022-11-08 10:32:55 +0300

    default image url changed and active status default added to Abstract user model on creation

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -84,19 +84,19 @@ ASGI_APPLICATION = 'mainproject.asgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #    "ENGINE" : "django.db.backends.postgresql_psycopg2",
-    #    'NAME': config("NAME", default=''),
-    #    'USER': config("USER", default=''),
-    #    'PASSWORD': config("PASSWORD", default=''),
-    #    'HOST': config("HOST", default=''),
-    #    'PORT': '5432'
-    # }
-
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+       "ENGINE" : "django.db.backends.postgresql_psycopg2",
+       'NAME': config("NAME", default=''),
+       'USER': config("USER", default=''),
+       'PASSWORD': config("PASSWORD", default=''),
+       'HOST': config("HOST", default=''),
+       'PORT': '5432'
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 
 }
 
