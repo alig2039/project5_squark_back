f09544ad1da647f14df7d4b21a1e96305aaf594d
alig2039 <alig2039@yahoo.com>
2022-11-10 11:21:44 +0300

    Changing smtp port to fix connection loss smtplib error

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -171,7 +171,7 @@ AUTH_USER_MODEL = 'users.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
